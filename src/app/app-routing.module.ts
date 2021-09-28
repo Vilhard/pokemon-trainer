8e96a038e1a3258a4e91558af2b4b60e995cd985
1l0v3m0un741n5 <>
2021-09-28 15:15:52 +0300

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { LoginPage } from './pages/login/page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: '/login',
+	},
+	{
+		path: 'login',
+		component: LoginPage
+	},
+	{
+		path: 'trainer',
+		component: TrainerPage
+	},
+	{
+		path: 'catalogue',
+		component: CataloguePage
+	}
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
