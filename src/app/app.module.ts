363dd32ead78bbd999a53e0439e9cf446f2b2e65
ville <ville.hotakainen@gmail.com>
2021-09-29 16:32:26 +0300

    Feat: display pokemon from with name and avatar in catalogue page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { LoginPage } from './pages/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonListItemComponent } from './components/pokemon-list-item/pokemon-list-item.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPage,
     TrainerPage,
-    CataloguePage
+    CataloguePage,
+    PokemonListComponent,
+    PokemonListItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,6 +1,8 @@
 export interface Pokemon {
+	id: number;
 	name: string;
 	url: string;
+	avatar: string;
 }
 
 export interface PokemonResults {
