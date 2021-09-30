7dc8e9a0ab6a52bc0237f1f48f8b56b714cce127
Peppi Mäkelä <peppi.makela@gmail.com>
2021-09-30 21:21:55 +0300

    Added "Free the Pokemon" button

--- src/app/components/trainer-pokemon-list-item/trainer-pokemon-list-item.component.ts
+++ src/app/components/trainer-pokemon-list-item/trainer-pokemon-list-item.component.ts
@@ -1,19 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { User } from 'src/app/models/user.model';
+import { SessionService } from 'src/app/services/session.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-trainer-pokemon-list-item',
   templateUrl: './trainer-pokemon-list-item.component.html',
   styleUrls: ['./trainer-pokemon-list-item.component.css'],
 })
-export class TrainerPokemonListItemComponent implements OnInit {
-  @Input() pokemon: Pokemon | undefined;
+export class TrainerPokemonListItemComponent {
+  @Input() pokemon!: Pokemon;
 
-  public onFreePokemonClicked(): void {
-    console.log('You have let the ' + this.pokemon?.name + ' free');
+  constructor(
+    private readonly userService: UserService,
+    private readonly sessionService: SessionService
+  ) {}
+
+  get user(): User {
+    return this.userService.getUser();
   }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  public onFreeClick(): void {
+    this.userService.updateUsersPokemons(
+      // Getting the current user
+      this.sessionService.user!,
+      // Filtering the deleted Pokemon  from the new array of pokemons
+      this.sessionService.user?.pokemon.filter(
+        (pokemon) => pokemon.id !== this.pokemon.id
+      )!,
+      async () => {
+        await console.log(this.sessionService.user);
+      }
+    );
+  }
 }
